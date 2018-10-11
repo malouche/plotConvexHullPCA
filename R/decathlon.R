8fa8df11dba3dbc2ee84d6f9fd9f50c4e693f11c
dhafer malouche <dhafer.malouche@yale.edu>
2018-10-11 16:45:01 +0100

    Add files via upload

---
+++ R/decathlon.R
@@ -0,0 +1,12 @@
+#' Performance in decathlon (data)
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format The data used here refer to athletes' performance during two sporting events.
+#' \describe{
+#'  A data frame with 41 rows and 13 columns: the first ten columns corresponds to the performance of the athletes for the 10 events of the decathlon. The columns 11 and 12 correspond respectively to the rank and the points obtained. The last column is a categorical variable corresponding to the sporting event (2004 Olympic Game or 2004 Decastar)
+#' }
+#' @usage  data(decathlon)
+#' @source Department of statistics and computer science, Agrocampus Rennes
+"decathlon"
